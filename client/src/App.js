ee58e2895807d62033ae222533143231bb176189
Akili1999 <sluground@gmail.com>
2019-08-14 18:27:51 -0400

    auth actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import NavBar from ' ./components/Navbar';
+
+import ForumList from './components/ForumList';
+
+import ForumModal from './components/ForumModal';
+
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import { loadUser } from './actions/authActions'
+
+import 'bootstrap/dist/css/boostrap.min.css'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store= {store}>
+        <div className='App'>
+          <NavBar>
+            <Container>
+              <ForumModal>
+                <ForumList>
+                  
+                </ForumList>
+              </ForumModal>
+            </Container>
+          </NavBar>
+        </div>
+      </Provider>
+    )
+  }
 }
+
 
 export default App;
