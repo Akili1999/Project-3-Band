9689723a1b4b152870f64d5dda74a2aad87bb2e5
Akili1999 <sluground@gmail.com>
2019-08-19 11:45:59 -0400

    building nav

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from 'react';
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+import { connect } from 'react-redux';
+
+import PropTypes from 'prop-types';
+
+import RegisterModal from './RegisterModal';
+
+import LoginModal from './LoginModal';
+
+import Logout from './Logout';
+
+class NavBar extends Component {
+    state = {
+        isOpen: false
+    };
+
+    static PropTypes = {
+        auth: PropTypes.object.isRequired
+    };
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
+
+    render() {
+        const { isAuthenticated, user } = this.props.auth;
+
+        const authLinks = (
+            <Fragment>
+                <NavItem>
+                    <span className='navbar-text mr-3'>
+                    <strong>{user ? `Welcome ${user.name}` : ''}</strong>
+                    </span>
+                </NavItem>
+                <NavItem>
+                    <Logout />
+                </NavItem>
+            </Fragment>
+        );
+        
+    }
+}
